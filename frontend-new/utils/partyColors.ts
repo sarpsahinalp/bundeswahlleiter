e68c3dc3e2d8759ef4e00d163555fbc5bc0d1bc8
sarpsahinalp <sarpsahinalp.official@gmail.com>
2025-01-29 18:08:03 +0100

    - Add party colors

---
+++ frontend-new/utils/partyColors.ts
@@ -0,0 +1,10 @@
+export const partyColors: { [key: string]: string } = {
+    "CDU": "#000000",
+    "CSU": "#000000",
+    SPD: "#E3000F",
+    AfD: "#009EE0",
+    FDP: "#FFED00",
+    "DIE LINKE": "#BE3075",
+    GRÜNE: "#64A12D",
+    Other: "#CCCCCC",
+  }  
